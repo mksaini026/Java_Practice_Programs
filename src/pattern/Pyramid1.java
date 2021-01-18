92fc80e1afd8adb62bc4fe078629ead1f9ecb74a
manoj kumar saini <mksaini026@gmail.com>
2021-01-18 23:12:52 +0530

    some Pyramid programs added

--- src/pattern/Pyramid1.java
+++ src/pattern/Pyramid1.java
@@ -9,7 +9,7 @@ public class Pyramid1 {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = n-i; j > 1; j--) {
+            for (int j = n - i; j > 1; j--) {
                 System.out.print(" ");
             }
             for (int k = 0; k <= i; k++) {
--- src/pattern/Pyramid5.java
+++ src/pattern/Pyramid5.java
@@ -2,14 +2,23 @@ package pattern;
 
 import java.util.Scanner;
 
-public class Pyramid4 {
+public class Pyramid5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = n - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i + i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k < 2 * n - 2 * i; k++) {
