e2ba50ff4a43617847c2160372df9307d183d63f
manoj kumar saini <mksaini026@gmail.com>
2021-01-18 22:27:56 +0530

    some Pyramid programs added

---
+++ src/pattern/Pyramid2.java
@@ -0,0 +1,21 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Pyramid2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n-i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i+i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
