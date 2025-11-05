99957dfb1595f82240f68be6e7a9f1ec8c202373
Manoj <mksaini026@gmail.com>
2025-11-05 18:24:47 +0530

    Added multiple classes for various functionalities including Node, Cow, and Pen

---
+++ src/com/test/CheckJava.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class CheckJava  extends Throwable
+{
+	public static void main(String[] args)
+	{	
+		int a=6, b=5;
+		double d = a/b;
+		System.out.println(d);
+		
+	}
+}
