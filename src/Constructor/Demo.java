24a1ee2f7dd7f0a85602932c942817bff00170ba
Manoj <mksaini026@gmail.com>
2025-11-05 19:01:46 +0530

    Refactor Demo and Test classes for improved structure and readability

--- src/Constructor/Demo.java
+++ src/Constructor/Demo.java
@@ -1,6 +1,12 @@
 package Constructor;
 
 public class Demo {
-	static  { System.out.println("Demo class static block");  }  void m1() { System.out.println("Demo class m1 method");  } 
+    static {
+        System.out.println("Demo class static block");
+    }
+
+    void m1() {
+        System.out.println("Demo class m1 method");
+    }
 
 }
--- src/Constructor/Test.java
+++ src/Constructor/Test.java
@@ -1,11 +1,15 @@
 package Constructor;
 
 public class Test {
-	public static void main(String[] args) throws Exception  
-	{ 
-		 new Demo().m1(); 
-		Class c = Class.forName("Demo");   
-		Demo d = (Demo)c.newInstance();   
-		d.m1(); 
 
-}}
+    public static void main(String[] args) throws Exception {
+        new Demo().m1();
+
+        //Class<?> c = Class.forName(Demo.class.getName()); or below line
+        Class<?> c = Class.forName("Constructor.Demo");
+        Demo d1 = (Demo) c.newInstance();   //deprecated Class.newInstance()
+        d1.m1();
+        Demo d = (Demo) c.getDeclaredConstructor().newInstance();
+        d.m1();
+    }
+}
